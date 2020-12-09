6bb3a530a29360c9d538064facf32c2a264e10a6
alumno <alumno@192.168.56.1>
2020-12-09 18:57:16 +0100

    FINAL

--- src/fp/daw/examen1ev/Ejercicio4.java
+++ src/fp/daw/examen1ev/Ejercicio4.java
@@ -9,8 +9,6 @@ public class Ejercicio4 {
 		// TODO Auto-generated method stub
 		
 		int[][] miArray = {{1,5,5,2,4},{2,1,4,14,3},{3,7,11,2,8},{4,8,12,16,4}}; 
-		System.out.println(miArray.length);
-		System.out.println(miArray[0].length);
 		int resultado = max3x3sum(miArray);
 		System.out.println("RESULTADO: " + resultado);
 	}
@@ -20,32 +18,40 @@ public class Ejercicio4 {
 		Random r= new Random();
 		int suma = 0;
 		
-		for (int i=0; i < miArray.length; i++) { // Relleno el array
+		// OPCION PARA RELLENAR EL ARRAY CON NUMEROS RANDOM
+		/*for (int i=0; i < miArray.length; i++) { // Relleno el array
 			for (int j=0; j<miArray[0].length; j++) {
-				//int n=Math.round(r.nextInt(10 -3))+3;
-				int n=Math.round(r.nextInt(Integer.MAX_VALUE -3))+3;
+				int n=Math.round(r.nextInt(10 -3))+3;
+				//int n=Math.round(r.nextInt(Integer.MAX_VALUE -3))+3;
 				miArray[i][j] = n;
 			}
 			System.out.print(Arrays.toString(miArray[i]));
-		}
-		int a=0, b=0, contadorA=0, contadorB=0;
-		while ((a < miArray.length)&& (contadorA<3)) {
-			while ((b < miArray[0].length)&& (contadorB<3)) {
-				suma += miArray[a][b];
-				contadorB++;
-				b++;
-				/*System.out.println("contadorB " + contadorB);
-				System.out.println("suma " + suma);*/
-				
-			}
-			contadorB = 0;
-			contadorA++;
-			a++;
-			/*System.out.println("contadorA " + contadorA);
-			System.out.println();
-			System.out.println(suma);*/
-			if (suma>resultado) {
-				resultado = suma;
+		}*/
+		int a=0, b=0, contadorA=0, contadorB=0, resultadoParcial=0;
+		while (a < miArray.length) {
+			if (contadorA==3) {
+				if (resultadoParcial>resultado) {
+					resultado = resultadoParcial;
+				}
+				contadorA=0;
+			} else {
+				while ((b < miArray[0].length)&& (contadorB<3)) {
+					suma += miArray[a][b];
+					contadorB++;
+					b++;
+					/*System.out.println("contadorB " + contadorB);
+					System.out.println("suma " + suma);*/
+					
+				}
+				contadorB = 0;
+				contadorA++;
+				a++;
+				/*System.out.println("contadorA " + contadorA);
+				System.out.println();
+				System.out.println(suma);*/
+				if (suma>resultadoParcial) {
+					resultadoParcial = suma;
+				}
 			}
 		}
 
